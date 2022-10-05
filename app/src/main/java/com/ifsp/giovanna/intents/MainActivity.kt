91ecab22741ab697f1a99b51415c91f87b2fc9c8
GiovannaDP <giovannadanellipau@gmail.com>
2022-10-05 20:07:23 -0300

    ajustando a chamada de uma página html para nao dar crash no app e continuidade nas chamadas dos itens do menu

--- app/src/main/java/com/ifsp/giovanna/intents/MainActivity.kt
+++ app/src/main/java/com/ifsp/giovanna/intents/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.ifsp.giovanna.intents
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
-import android.content.Intent.ACTION_DIAL
-import android.content.Intent.ACTION_VIEW
+import android.content.Intent.*
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -21,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var urlArl: ActivityResultLauncher<Intent>
+    private lateinit var permissaoChamadaArl: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +40,19 @@ class MainActivity : AppCompatActivity() {
                 activityMainBinding.urlTv.text = urlRetornada
             }
         }
+
+        permissaoChamadaArl = registerForActivityResult(ActivityResultContracts.RequestPermission(),
+        object: ActivityResultCallback<Boolean>{
+            //unit é o mesmo que void
+            override fun onActivityResult(permissaoConcedida: Boolean?) {
+                if (permissaoConcedida!!){
+                    chamarNumero((true))
+                }else {
+                    Toast.makeText(this@MainActivity, "Permissão necessária para execução", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        })
 
         activityMainBinding.entrarUrlBt.setOnClickListener {
             // chamada de outra tela
@@ -54,21 +72,51 @@ class MainActivity : AppCompatActivity() {
 
     // trata das escolhas das opcoes do meny
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //when similar ao switch case
         return when (item.itemId) {
             R.id.viewMi -> {
                 //abrir navegador na url digitada pelo usuario
-                val url = Uri.parse(activityMainBinding.urlTv.text.toString())
-                val navegadorIntent = Intent(ACTION_VIEW, url)
-                startActivity(navegadorIntent)
+                val url = activityMainBinding.urlTv.text.toString()
+
+                if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                    Toast.makeText(this, "Url inválida", Toast.LENGTH_SHORT).show()
+                    false
+                }else{
+                    val uri = Uri.parse(url)
+                    val navegadorIntent = Intent(ACTION_VIEW, uri)
+                    startActivity(navegadorIntent)
+                    true
+                }
+            }
+
+            R.id.dialMi -> {
+                chamarNumero(false)
                 true
             }
-            R.id.dialMi -> {
-                val uri = Uri.parse("tel: ${activityMainBinding.urlTv.text}")
-                val chamarIntent = Intent(ACTION_DIAL, uri)
-                startActivity(chamarIntent)
+
+            R.id.callMi -> {
+                //verificar a versao do android
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    //se superior ou igual Marshmallow
+                        //verificar se tem permissao e solicitar se for necessario
+                    if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED){
+                        //fazer a chamada
+                        chamarNumero(true)
+                    } else {
+                        // solicitar permissao
+
+                    }
+                }
                 true
             }
             else -> { false }
         }
+    }
+
+    private fun chamarNumero(chamar: Boolean){
+        val uri = Uri.parse("tel: ${activityMainBinding.urlTv.text}")
+        val intent = Intent(if (chamar) ACTION_CALL else ACTION_DIAL)
+        intent.data = uri
+        startActivity(intent)
     }
 }
